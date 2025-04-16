80ae9d0227a27477583f47c9e9a9294f9b4f6034
EnricVB <enricvbufi@gmail.com>
2025-04-16 15:58:36 +0200

    Feat: Added nav link to Header

--- src/components/website/Header.tsx
+++ src/components/website/Header.tsx
@@ -7,17 +7,23 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerTitle">
-                <img className="icon headerIcon" src={trackitIcon} />
-                <h1 className="headerTitle headerIcon">Trackit</h1>
+                <nav>
+                    <Link to="/"><img className="icon headerIcon" src={trackitIcon} /></Link>
+                </nav>
+
+                <nav >
+                    <Link to="/" style={{textDecoration: "none"}}><h1 className="headerTitle headerIcon">Trackit</h1></Link>
+                </nav>
+
                 <h1 className="headerSubtitle">Sistema de Control de Versiones</h1>
-                
+
                 {/* Botón de descarga en el Header */}
                 <div className="buttonsInHeader">
                     <nav>
                         <Link to="/download">Descargar Trackit</Link>
                     </nav>
                     <nav>
-                        <Link to="/">Más sobre Trackit</Link>
+                        <Link to="/commandguide">Más sobre Trackit</Link>
                     </nav>
                 </div>
             </div>
